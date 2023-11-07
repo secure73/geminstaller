657be7533d1db32ebce1884106c420bb09ed12f6
secure73 <secure73@gmail.com>
2023-11-07 17:59:55 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use GemLibrary\Helper\NoCors;
+use GemFramework\Core\Bootstrap;
+use GemLibrary\Http\ApacheRequest;
+
+NoCors::NoCors();
+$serverRequest = new ApacheRequest();
+$bootstrap = new Bootstrap($serverRequest->request);
