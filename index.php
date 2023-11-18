cf0224f372f8a7a424efb1f35d646fc44541a692
Roboxon <145359440+roboxon@users.noreply.github.com>
2023-11-18 23:42:22 +0100

    set to gemLibraray v2 and gem framework v2

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 use GemLibrary\Helper\NoCors;
 use GemFramework\Core\Bootstrap;
 use GemLibrary\Http\ApacheRequest;
